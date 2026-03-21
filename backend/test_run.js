463a3030e5bfbbb30e6874b6c77ebb5f1bffe57b
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-03-21 06:20:00 +0530

    fix: UI bugs

--- backend/test_run.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-import('./server.js').catch(e => {
-  fs.writeFileSync('err.txt', e.stack || String(e));
-});
--- react/src/services/api.js
+++ react/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://hackindia-spark-4-ncr-east-region-byte.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
